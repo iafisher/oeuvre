762e4d08040bbd7d11889077f284fe0ae43d7ffd
Ian Fisher <iafisher@protonmail.com>
2020-05-17 17:51:39 -0700

    Set up precommit.py

---
+++ precommit.py
@@ -0,0 +1,20 @@
+"""Pre-commit configuration for git.
+
+This file was created by precommit (https://github.com/iafisher/precommit).
+You are welcome to edit it yourself to customize your pre-commit hook.
+"""
+from precommitlib import checks
+
+
+def init(precommit):
+    # Generic checks
+    precommit.check(checks.NoStagedAndUnstagedChanges())
+    precommit.check(checks.NoWhitespaceInFilePath())
+    precommit.check(checks.DoNotSubmit())
+
+    # Language-specific checks
+    precommit.check(checks.PythonFormat())
+    precommit.check(checks.PythonStyle(args=["--extend-ignore=E731"]))
+    precommit.check(
+        checks.Command("PythonTypes", ["mypy"], pass_files=True, pattern=r"^oeuvre.py$")
+    )
